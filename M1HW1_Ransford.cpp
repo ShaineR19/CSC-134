3457159cb415e2362a7a54804ce432e28f960018
Shaine Ransford <159803141+ShaineR19@users.noreply.github.com>
2025-01-27 16:51:42 +0000

    M1HW1 second commit

--- M1HW1_Ransford.cpp
+++ M1HW1_Ransford.cpp
@@ -19,6 +19,13 @@ double grossTotal = 321.5;
 
 // print all the information about the movie
 cout << "The movie " << name << " was released in " << year 
-<< " and grossed a total of " << grossTotal << " million.";
+<< " and grossed a total of " << grossTotal << " million." << endl;
+cout <<" " << endl;
+cout << "About: In Nazi-occupied France during World War II, " 
+<< "a plan to assassinate Nazi leaders" << endl << "by a group of Jewish " 
+<< "U.S. soldiers coincides with a theatre owner's vengeful plans for the same."<< endl;
+cout <<" " << endl;
+cout << name << " has a stellar cast featuring, Brad Pitt, Christoph Waltz, Eli Roth and Michael Fassbender." << endl;
+cout << "Quentin Tarantino is credited as the writer and director for "<< name << endl;
 
 }
