45541d1768ad25aa0576bc824a4bc8a696f289fc
Shaine Ransford <159803141+ShaineR19@users.noreply.github.com>
2025-01-27 16:37:08 +0000

    M1HW1 first commit

---
+++ M1HW1_Ransford.cpp
@@ -0,0 +1,24 @@
+// CSC-134
+// M1HW1
+// Shaine Ransford
+// 1/27/2025
+// This program will talk about a movie.
+
+#include <iostream> 
+using namespace std;
+
+int main()
+{
+// The owner’s name
+string name = "Inglorious Basterds";
+
+// number of apples owned
+int year = 2009;
+// price per apple
+double grossTotal = 321.5;
+
+// print all the information about the movie
+cout << "The movie " << name << " was released in " << year 
+<< " and grossed a total of " << grossTotal << " million.";
+
+}
