6dad9e90e97cae5ff3018c2e3d0ed23895ee9bef
Shaine Ransford <159803141+ShaineR19@users.noreply.github.com>
2025-01-29 16:15:32 +0000

    m2t2 first commit setprecision

---
+++ M2T2.cpp
@@ -0,0 +1,33 @@
+// CSC-134
+// M1LAB
+// Shaine Ransford
+// 1/27/2025
+// This program will print a receipt for a transaction
+
+#include <iostream> 
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    // Declare the variables.
+    // Calculate the tax amount.
+    // Add the tax amount to the total.
+    // Display the total to the user.
+
+    // Variables
+    const double tax = 0.08;
+    double meal_price = 5.99;
+    double total;
+
+    // Calculations
+    double tax_amount = meal_price * tax;
+    total = tax_amount + meal_price;
+
+    // Output
+    cout << setprecision(2)<< fixed 
+         << "Price: "<< meal_price<< endl
+         << "  Tax: "<< tax_amount<< endl
+         << "Total: "<< total<< endl;
+          
+}
