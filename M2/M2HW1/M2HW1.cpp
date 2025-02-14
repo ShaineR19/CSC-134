4b948d2eafd49991fa65afede1a93ead481024a8
Shaine Ransford <159803141+ShaineR19@users.noreply.github.com>
2025-02-14 00:01:48 +0000

    m2hw1 g2

--- M2/M2HW1/M2HW1.cpp
+++ M2/M2HW1/M2HW1.cpp
@@ -5,6 +5,7 @@
 
 #include <cstdlib>
 #include <iostream> 
+#include <iomanip>
 using namespace std;
 
 int main() 
@@ -55,11 +56,45 @@ int main()
     // Question 2
     cout << "Question 2"<< endl;
     cout << "" << endl;
+
+    // CONSTANT VARIABLES
+    const double COST_PER_CUBIC_FOOT = 0.3;
+    const double CHARGE_PER_CUBIC_FOOT = 0.52; 
+
     // Variables
+    double  length, // The crate's length
+            width,  // The crate's width
+            height, // The crate's height
+            volume, // The volume of the crate
+            cost,   // The cost to build the crate
+            charge, // The customer charge for the crate
+            profit; // The profit made on the crate
 
-    // Calculations
+    // Set the desired output formatting for numbers.
+    cout << setprecision(2) << fixed << showpoint; 
 
-    // Ouput
+    // Prompt the user for crate's length, width, and height
+    cout << "Enter the dimensions of the crate (in feet):\n";
+    cout << "Length: ";
+    cin >> length;
+    cout << "Width: ";
+    cin >> width;
+    cout << "Height: ";
+    cin >> height;
+
+    // Calculate the crate's volume, the cost to produce it,
+    // the charge to the customer, and the profit. 
+    volume = length * width * height; 
+    cost = volume * COST_PER_CUBIC_FOOT;
+    charge = volume * CHARGE_PER_CUBIC_FOOT;
+    profit = charge - cost; 
+
+    // Display the calculated data.
+    cout << "The volume of the crate is ";
+    cout << volume << " cubic feet.\n"; 
+    cout << "Cost to build: $" << cost << endl;
+    cout << "Charge to customer: $" << charge << endl;
+    cout << "Profit: $" << profit << endl;
     cout << "" << endl;
 
     // Question 3
