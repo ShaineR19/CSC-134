b5e3676d5c457ce5a527d34dfd7cbc51099d16b2
Shaine Ransford <159803141+ShaineR19@users.noreply.github.com>
2025-02-24 16:01:03 +0000

    m3lab final final

--- M3/M3T2/M3Lab/M3Lab.cpp
+++ M3/M3T2/M3Lab/M3Lab.cpp
@@ -10,47 +10,64 @@ using namespace std;
 
 void leftPath();
 void rightPath();
+void straightPath();
 
 int main() 
 {
     // Variables
     string choice; 
-
-    // Input
-    cout << "Welcome to the Dungeon" << endl;
-    cout << "Do you want to go Left or Right?" << endl;
-    cout << "Type Left or Right: "; 
-    cin >> choice;
+    string keep_going = "Yes";
   
-    if (choice == "Left") 
+    cout << "Welcome to the Dungeon" << endl;
+    while (keep_going == "Yes")
     {
-        leftPath();
-    }
-    else if (choice == "Right") 
-    {
-        rightPath();
-    }
-    else 
-    {
-        cout << "I'm sorry, that is not a valid choice." << endl;
-    }
+        cout << endl << "Do you want to go Left or Right?" << endl;
+        cout << "Type Left, Right or Straight: "; 
+        cin >> choice;
 
+        if (choice == "Left") 
+        {
+            leftPath();
+        }
+        else if (choice == "Right") 
+        {
+            rightPath();
+        }
+        else if (choice == "Straight")
+        {
+            straightPath();
+        }
+        else 
+        {
+            cout << endl << "I'm sorry, that is not a valid choice." << endl;
+        }
+
+        cout << endl << "Do you want to keep going? ('Yes' to continue)";
+        cin >> keep_going;
+    }
     cout << endl << "Thank you for playing!" << endl;
-    return 0; // tells the computer that we finished without errors
+        return 0; // tells the computer that we finished without errors
 } 
 
 void leftPath() 
 {
-    // this function is called in main if the user chooses 1.
-    cout << endl << "You went Left" << endl;
+    // this function is called in main if the user chooses "Left".
+    cout << endl << "You went Left!" << endl;
     cout << "You found a Diamond!" << endl;
 }
 
 void rightPath() 
 {
-    // this function is called in main if the user chooses 'right.
-    cout << endl << "You went Right" << endl;
+    // this function is called in main if the user chooses "Right".
+    cout << endl << "You went Right!" << endl;
     cout << "You found a dirty Rat!" << endl;
+}
+
+void straightPath() 
+{
+    // this function is called in main if the user chooses "Straight".
+    cout << endl << "You went Straight!" << endl;
+    cout << "You found a Singing Cockroach!" << endl;
 }
 
 
