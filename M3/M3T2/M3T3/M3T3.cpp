a1ec49d6fcf5e7b3a02c0d09e8ae1dd5f6adf0a9
Shaine Ransford <159803141+ShaineR19@users.noreply.github.com>
2025-02-26 21:15:21 +0000

    m3t3 final

--- M3/M3T2/M3T3/M3T3.cpp
+++ M3/M3T2/M3T3/M3T3.cpp
@@ -1,33 +1,54 @@
 // Shaine Ransford
 // 2/24/2025
-// M3Lab
+// M3T3
 // CSC-134
+// RNG Game
 
 #include <iostream>
 #include <string>
+#include <cmath>
+#include <ctime>
 
 using namespace std;
 
 int roll();
 
 int main() 
-{
+{   
     // Variables
-    string choice; 
-    string keep_going = "Yes";
-  
-    cout << "Welcome to the Dungeon" << endl;
-    while (keep_going == "Yes")
-    {
-        cout << endl << "Do you want to go Left or Right?" << endl;
-        cout << "Type Left, Right or Straight: "; 
-        cin >> choice;
+    int seed = time(0);
+    int total;
+    srand(seed);
 
-        
+    // Roll Dice
+    int roll1 = roll();
+    int roll2 = roll(); 
+    total = roll1 + roll2;
 
-        cout << endl << "Do you want to keep going? ('Yes' to continue)";
-        cin >> keep_going;
+    // Output
+    cout << "Let's roll some dice!" << endl;
+    cout << endl << "You rolled " << roll1 << " and "<< roll2 << endl;
+    cout << "Point Total: " << total << endl;
+    cout << "" << endl;
+
+    // Let's play craps! Results*
+    if (total == 7 || total == 11) 
+    {   // roll is 7 or 11
+        cout << "Winner Winner Chicken Dinner!" << endl;
     }
-    cout << endl << "Thank you for playing!" << endl;
-        return 0; // tells the computer that we finished without errors
-} 
+    else if (total == 2 || total == 3 || total == 12) 
+    {   // roll is 2, 3, or 12
+        cout << "Snake eyes! Too bad, you lose." << endl;
+    }
+    else 
+    {   // roll is anything else
+        cout << "Your point is " << total << " but we'll do that part later" << endl;
+    }
+
+    return 0;
+}
+
+int roll()
+{
+    return (rand() % 6) + 1;    
+}
