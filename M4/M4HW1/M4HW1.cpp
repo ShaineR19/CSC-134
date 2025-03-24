c4fe4884b3be07b497347aef268ed72b5db9d422
Shaine Ransford <159803141+ShaineR19@users.noreply.github.com>
2025-03-24 16:55:03 +0000

    m4hw1 final

--- M4/M4HW1/M4HW1.cpp
+++ M4/M4HW1/M4HW1.cpp
@@ -7,7 +7,46 @@
 #include <iomanip>
 using namespace std;
 
-int main() 
-{   
 
+int main() 
+{
+    // Loop, user input variable are set before the while loop so they are not iterated over twice
+    int number;
+    bool invalid = true;
+
+    // Input validation loop
+    while (invalid)
+    {
+        cout << "Enter a number from 1 to 12: ";
+        cin >> number;
+
+        if (cin.fail())
+        {
+            cin.clear(); // clear error flags
+            cin.ignore(1000, '\n'); // discard input
+        }
+        else if (number < 1 || number > 12) 
+        {
+            cout << "Please enter a valid number between 1 and 12." << endl;
+        } 
+        else 
+        {
+            cout << "Printing your times table for "<< number << ". " << endl;
+            invalid = false;
+        }
+    }
+
+    // Counter, table_size variable are set before the while loop so they are not iterated over twice
+    int count = 1;
+    int table_size = 12;
+
+    // Print times table for the chosen number up to 12 
+    while (count <= table_size) 
+    {
+        int result = number * count;
+        cout << number << " times " << count << " is " << result << "." << endl;
+        count++;
+    }
+
+    return 0;
 }
