a6c3e8c8700b44eeb327223f2a6e8fcf225b272c
Shaine Ransford <159803141+ShaineR19@users.noreply.github.com>
2025-03-26 18:02:52 +0000

    m5t2

--- M5/M5T2.cpp
+++ M5/M5T2.cpp
@@ -7,7 +7,34 @@
 #include <iomanip>
 using namespace std;
 
+void square(int num1);
+void print_answer(int value1, int value);
+
 int main() 
 {
-    printf("Hello World");
+    printf("Hello User\n");
+    printf("Lets square some numbers!\n");
+
+    printf("\nWhat is your favorite number: ");
+    int max;
+    cin >> max;
+
+    for (int i = 0; i <= max; i++)
+    {
+        printf("");
+        square(i);
+    }
+
+    return 0;
+}
+
+void square(int num)
+{
+    int value = num * num;
+    print_answer(num, value);
+}
+
+void print_answer(int num, int value)
+{
+    printf("%i - Square Value: %i\n", num, value);
 }
