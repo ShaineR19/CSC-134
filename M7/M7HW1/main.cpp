a417eef8788e5ac24632fb32f555dfcfd5fd385a
Shaine Ransford <159803141+ShaineR19@users.noreply.github.com>
2025-05-13 19:26:28 +0000

    main

---
+++ M7/M7HW1/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include "player.h"
+#include "enemy.h"
+#include "item.h"
+
+int main() {
+    Player player("Artemis");
+    Enemy goblin("Goblin", 30, 8, 2);
+
+    Item potion("Healing Potion", "potion", 20);
+    player.addItem(potion);
+
+    std::cout << "\nA wild " << goblin.getType() << " appears!\n";
+
+    while (player.isAlive() && goblin.isAlive()) {
+        std::cout << "\n-- Player Turn --\n";
+        player.displayStats();
+
+        std::string choice;
+        std::cout << "Choose action: (attack / use potion): ";
+        std::cin >> choice;
+
+        if (choice == "attack") {
+            goblin.takeDamage(player.getAttack());
+        } else if (choice == "use") {
+            std::string itemName;
+            std::cin.ignore();
+            std::getline(std::cin, itemName);
+            player.useItem(itemName);
+        }
+
+        if (!goblin.isAlive()) break;
+
+        std::cout << "\n-- Enemy Turn --\n";
+        player.takeDamage(goblin.getAttack());
+    }
+
+    if (player.isAlive()) {
+        std::cout << "\nYou defeated the " << goblin.getType() << "!\n";
+    } else {
+        std::cout << "\nYou were defeated in battle...\n";
+    }
+
+    return 0;
+}
